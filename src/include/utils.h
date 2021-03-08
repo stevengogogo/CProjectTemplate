034025e00d334fbb71c0c5630d72c704f3d0d1dd
stevengogogo <stevengogogo4321@gmail.com>
2021-03-08 14:04:53 +0800

    test doxygen comment

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,6 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/**
+ * @brief Add two points
+ *
+ * @param extreme
+ * @param arg
+ * @return struct Point
+ */
 typedef struct {
     int extreme;
     int arg;
